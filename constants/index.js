3e00b6227f1b44bade8590e1bd3c8b029c133685
SergeyShkredow <1978tarf@gmail.com>
2020-05-13 11:55:16 +0300

    base commit

---
+++ constants/index.js
@@ -0,0 +1,10 @@
+export const USER_PASS = '!Qwerty123#'
+export const USER_LOGIN = 'test04052001'
+export const USER_EPICSKINS = 'hdb27507'
+
+
+export default {
+    USER_EPICSKINS,
+    USER_LOGIN,
+    USER_PASS
+}
